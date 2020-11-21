672674c430ae2ba0f1aace27112ec02c9a55c3ba
ralamo33 <ralamo33@gmail.com>
2020-11-21 18:40:13 -0500

    Home page improvement

--- src/layouts/layout.js
+++ src/layouts/layout.js
@@ -1,9 +1,12 @@
 import React from "react"
 import { createGlobalStyle } from "styled-components"
+import * as Constants from "../constants";
+
 const GlobalStyle = createGlobalStyle`
   body {
     background: ${props => (props.theme === "medieval" ? "#E8D8A6" : "#3d3d5c")};
     color: ${props => (props.theme === "medieval" ? "black" : "white")};
+    background-image: url(${Constants.BUCKET_URL + "Scroll.jpg"});
     font-family: 'MedievalSharp', cursive;
     font-weight: 400;
   }
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,9 @@ import AlternateSides from "../layouts/AlternateSides";
 import Layout from "../layouts/layout";
 import Title from "../components/Title";
 import MyNavbar from "../components/MyNavbar";
+import RowHeaderText from "../components/RowHeaderText";
+import { Container, Row, Col} from "react-bootstrap";
+import * as Constants from "../constants";
 
 export default function Home() {
   const navItems=['Kickstarter', 'Our Journey'];
@@ -11,12 +14,11 @@ export default function Home() {
     <Layout>
       <Title title="Simple Tavern"></Title>
       <MyNavbar navItems={navItems}></MyNavbar>
-      <AlternateSides>
-        <h1>Simple</h1>
-        <p>This is super simple</p>
-        <p>This is super simple</p>
-        <h1>Modular</h1>
-      </AlternateSides>
+      <Container fluid>
+        <RowHeaderText header="Simple" color="yellow" text={Constants.SIMPLE_TEXT} headerFirst={true}></RowHeaderText>
+        <RowHeaderText header="Modular" color="blue" text="This is pretty simple" headerFirst={false}></RowHeaderText>
+        <RowHeaderText header="Customizable" color="red" text="This is pretty simple" headerFirst={true}></RowHeaderText>
+      </Container>
     </Layout>
   )
 }
