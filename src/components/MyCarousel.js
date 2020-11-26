7727edae824c9e0f153f13bec40457e83d464d01
ralamo33 <ralamo33@gmail.com>
2020-11-26 12:05:34 -0500

    hello

--- src/components/MyCarousel.js
+++ src/components/MyCarousel.js
@@ -8,17 +8,17 @@ export default function MyCarouselItem(props) {
    return (
         <Carousel>
             <Carousel.Item>
-                <MyOverlayTrigger url="https://i.pinimg.com/originals/45/46/7e/45467e47f8c1404816d60859c966cb10.jpg"
+                <MyOverlayTrigger url={Constants.BUCKET_URL + 'Chase.jpg'}
                  color="yellow" header="Fast-Paced" text={Constants.SIMPLE_TEXT}>
                 </MyOverlayTrigger>
             </Carousel.Item>
             <Carousel.Item>
-                <MyOverlayTrigger url="https://i.pinimg.com/originals/df/59/76/df5976ef387c9a6954fcab9185af763b.jpg"
+                <MyOverlayTrigger url={Constants.BUCKET_URL + 'Cavern.jpg'}
                  color="red" header="Intense" text={Constants.INTENSE_TEXT}>
                 </MyOverlayTrigger>
             </Carousel.Item>
             <Carousel.Item>
-                <MyOverlayTrigger url="https://i.pinimg.com/originals/b5/b2/9f/b5b29f5fbf39373627c27cd8151f9bb3.jpg"
+                <MyOverlayTrigger url={Constants.BUCKET_URL + 'Workshop.jpg'}
                  color="#ccffff" header="Customizable" text={Constants.CUSTOMIZABLE_TEXT}>
                 </MyOverlayTrigger>
             </Carousel.Item>
