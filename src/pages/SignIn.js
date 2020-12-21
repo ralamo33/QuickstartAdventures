bbe12de12cc9cd76d2dedb67e6b74eb67686fc82
ralamo33 <ralamo33@gmail.com>
2020-12-21 12:35:39 -0500

    Adding authentication from amplify

---
+++ src/pages/SignIn.js
@@ -0,0 +1,6 @@
+import React from "react"
+import {AmplifyAuthenticator} from "@aws-amplify/ui-react";
+
+export default function() {
+    return <AmplifyAuthenticator></AmplifyAuthenticator>
+}
---
+++ src/pages/SignOut.js
@@ -0,0 +1,6 @@
+import React from "react"
+import {AmplifySignout} from "@aws-amplify/ui-react";
+
+export default function() {
+    return <AmplifySignout></AmplifySignout>
+}
