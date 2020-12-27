d1012e051058b84d75981572a0bf233403b70187
Ryan Alamo <ralamo@pop-os.localdomain>
2020-12-27 06:41:39 -0500

    Onwards works

--- src/components/MyModal.js
+++ src/components/MyModal.js
@@ -2,19 +2,29 @@ import React, {useState} from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import {Container, Row, Col, Button, Modal, Form } from "react-bootstrap";
 import EmailForm from "../components/EmailForm";
+import axios from "axios";
 
 
 
 export default function MyModal(props) {
   const show = props.show;
   const setShow = props.setShow;
-
+  const apiUrl = 'https://cors-anywhere.herokuapp.com/https://9oeq1w1vcf.execute-api.us-east-1.amazonaws.com/prod/notify';
+  const emailParam = '?TopicArn=arn:aws:sns:us-east-1:451835830300:AddEmail&Message=';
+  let [email, setEmail] = useState("");
+  
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-
-  let email = "";
-
-  const handleSubmit = (event) => alert(email)
+  
+  const handleSubmit = event => {
+    event.preventDefault();
+    axios.post(
+      apiUrl + emailParam + email, {
+        headers: {"Access-Control-Allow-Origin": "*"}
+      }
+    ).then(handleClose()
+    ).catch((error) => alert(error));
+  }
 
   return (
     <div>
@@ -32,7 +42,7 @@ export default function MyModal(props) {
           <Form onSubmit={handleSubmit}>
               <Form.Group controlId="EmailSubmit">
                   <Form.Label>Email Address</Form.Label>
-                  <Form.Control type="email" placeholder="Enter email"/>
+                  <Form.Control type="email" placeholder="Enter email" value={email} onChange={(event) => setEmail(event.target.value)}/>
                   <Form.Text>
                       This is where we will email you a pdf of Quickstart Adventures. We'll never share your email with anyone else.
                   </Form.Text>
