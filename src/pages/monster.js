892899f27284a60c8025734f9e032aff0ed9946d
sean.hollen <hollen.s@husky.neu.edu>
2021-04-28 04:35:49 -0400

    monster page image upload

--- src/pages/monster.js
+++ src/pages/monster.js
@@ -21,9 +21,30 @@ const Title = styled.p`
   color: #ccffff;
 `;
 
+const LabelWhite = styled.label`
+  color: white; 
+  text-align: center; 
+`;
+
+const InputWhite = styled.input`
+  color: white; 
+  margin: auto;
+  width: 250px; 
+  text-align: center;
+`;
+
 const NameInput = styled.input`
   width: 350px;
 `;
+
+let fileReader;
+
+const onLoadend = (e) => {
+  console.log("getting content");
+  const content = fileReader.result;
+  console.log("content:")
+  console.log(content);
+}
 
 export default class Monster extends React.Component {
 
@@ -37,7 +58,8 @@ export default class Monster extends React.Component {
 
   submit = (event) => {
     event.preventDefault();
-    apiUrl = `${Constants.MONSTER_API}&Message=${this.state.name}_${this.state.about}`;
+    apiUrl = `${Constants.MONSTER_API}&Message=${this.state.name}_${this.state.about}
+    &TopicArn=${Constants.MONSTER_TOPIC_ARN}&UploadFile=${fileReader}`;
     axios.post(
       apiUrl, {}, {
         headers: { 'x-api-key': Constants.MONSTER_API_KEY },
@@ -54,6 +76,14 @@ export default class Monster extends React.Component {
   setAbout = (event) => {
     this.setState({about: event.target.value});
   }
+  
+  readFile = (file) => {
+    console.log("loading file"); 
+    fileReader = new FileReader();
+    fileReader.onload = onLoadend;
+    fileReader.readAsArrayBuffer(file);
+    console.log("done");
+  };
 
   render() {
 
@@ -62,13 +92,19 @@ export default class Monster extends React.Component {
         <Title>Create a Monster</Title>
         <Relative>
           <Form onSubmit={this.submit}>
-            <br /><br /><br/>
+            <br/><br/><br/>
             <label>
               <NameInput placeholder="Monster Name" value={this.state.name} onChange={this.setName} />
             </label>
-            <br /><textarea id="w3review" name="w3review" rows="4" cols="50" placeholder="Monster Description"
+            <br/>
+              <LabelWhite htmlFor="img"> Upload image: </LabelWhite>
+              <InputWhite id="img" name="img" type="file" accept="image/*" 
+              onChange={e => this.readFile(e.target.files[0])}/>
+            <br/>
+            <textarea id="w3review" name="w3review" rows="4" cols="50" placeholder="Monster Description"
               value={this.state.about} onChange={this.setAbout} />
-            <br /><input type="submit" value="Onwards" />
+            <br/>
+            <input type="submit" value="Submit!" />
           </Form>
           <EpicImage src="https://pop-verse.com/wp-content/uploads/2015/05/Abzan-Houses-MtG-Art-620x330.jpg" />
         </Relative>
