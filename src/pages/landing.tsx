e0ad3ee3cb5de6d68f3f45eff720ee42efbc8d74
Ryan Alamo <ralamo@pop-os.localdomain>
2021-06-09 07:04:43 -0400

    a

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -5,13 +5,12 @@ import Layout from '../layouts/layout';
 import LandingForm from '../components/Forms/LandingForm';
 import BackgroundImageContainer from '../components/BackgroundImageContainer';
 
-
 export default function Landing(): ReactElement {
   return (
     <Layout>
-      <BackgroundImageContainer >
-      <Break height="20vh"></Break>
-              <LandingForm></LandingForm>
+      <BackgroundImageContainer>
+        <Break height="20vh"></Break>
+        <LandingForm></LandingForm>
       </BackgroundImageContainer>
     </Layout>
   );
