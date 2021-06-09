80cec973c9bb99647b0fd687b4d7a49e70be59de
Ryan Alamo <ralamo@pop-os.localdomain>
2021-06-09 07:03:03 -0400

    Got eslint working again

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,6 @@ module.exports = {
   extends: [
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
   ],
   settings: {
--- src/components/Forms/FreeForm.tsx
+++ src/components/Forms/FreeForm.tsx
@@ -1,6 +1,6 @@
 import React, { ReactElement, useState } from 'react';
-import styled, {Keyframes, keyframes} from 'styled-components';
-import { Formik, Form, FormikErrors, FormikTouched, FormikState } from 'formik';
+import styled, { keyframes } from 'styled-components';
+import { Formik, Form, FormikState } from 'formik';
 import * as Yup from 'yup';
 import * as Constants from '../../constants';
 import { post } from '../../utils';
@@ -8,49 +8,26 @@ import PrettyButton from '../Buttons/PrettyButton';
 import TextField from '../FormFields/TextField';
 import * as Animations from 'react-animations';
 
-
 interface FormValues {
   email: string;
 }
-
-const border = `2.2px solid ${Constants.BLUE};`
-
-const PrettyText = styled.p`
-  font-size: 1.5rem;
-  text-align: center;
-  max-width: 30vw;
-  display: block;
-  margin: 10px auto;
-`;
 
 interface AnimationProps {
   animPlay: string;
 }
 
 const pulseAnimation = keyframes`${Animations.pulse}`;
-const fadeAnimation = keyframes`${Animations.fadeIn}`;
 
 const Animation = styled.div`
   animation: 1s ${pulseAnimation};
   animation-play-state: ${(props: AnimationProps) => props.animPlay};
 `;
 
-const ErrorText = styled(PrettyText)`
-  color: white;
-  background: red;
-`;
-
-const SuccessText = styled(PrettyText)`
-  color: white;
-  background: ${Constants.BLUE};
-`;
-
 export default function FreeForm(): ReactElement {
-
   const startBorderColor = Constants.BLUE;
   const startBorderWidth = '2.2px';
   const startPlacedholder = 'Email Address';
-  const updatedBorderColor = '#66ff66'
+  const updatedBorderColor = '#66ff66';
   const updatedBorderWidth = '5px';
   const updatedPlacedholder = 'Success!';
 
@@ -58,7 +35,6 @@ export default function FreeForm(): ReactElement {
   const [borderWidth, setBorderWidth] = useState(startBorderWidth);
   const [play, setPlay] = useState('paused');
   const [placeholder, setPlaceholder] = useState(startPlacedholder);
-
 
   const ValidationSchema = Yup.object().shape({
     email: Constants.STRING_VALIDATION.email('Invalid email'),
@@ -69,14 +45,16 @@ export default function FreeForm(): ReactElement {
     setBorderColor(updatedBorderColor);
     setPlay('start');
     setBorderWidth(updatedBorderWidth);
-  }
+  };
 
-  const onSubmit = (values: FormValues, resetForm:(nextState?: Partial<FormikState<any>>) => void) => {
-    post(
-      `${Constants.LANDING_API}&Message=John Doe_${values.email}`
-    ).then(updateEmailField).then(() => resetForm({}))
-  }
-
+  const onSubmit = (
+    values: FormValues,
+    resetForm: (nextState?: Partial<FormikState<any>>) => void
+  ) => {
+    post(`${Constants.LANDING_API}&Message=John Doe_${values.email}`)
+      .then(updateEmailField)
+      .then(() => resetForm({}));
+  };
 
   return (
     <div>
@@ -85,32 +63,30 @@ export default function FreeForm(): ReactElement {
           email: '',
         }}
         validationSchema={ValidationSchema}
-        onSubmit={(values, {resetForm}) => onSubmit(values, resetForm)}
+        onSubmit={(values, { resetForm }) => onSubmit(values, resetForm)}
       >
-        {(errors: FormikErrors<string>, touched: FormikTouched<string>) => (
-          <Form>
-            <Animation animPlay={play}>
-              <TextField
-                name="email"
-                placeholder={placeholder}
-                minWidth="25vw"
-                borderColor={borderColor}
-                borderWidth={borderWidth}
-                fontSize="1.5rem"
-              />
-            </Animation>
-            <PrettyButton
-              variant="warning"
-              type="submit"
-              fontSize="2rem"
-              color="white"
-              backgroundcolor="#800000"
-              bordercolor="#800000"
-              hovercolor="#e60000"
-              text={Constants.FREE_BUTTON_TEXT}
+        <Form>
+          <Animation animPlay={play}>
+            <TextField
+              name="email"
+              placeholder={placeholder}
+              minWidth="25vw"
+              borderColor={borderColor}
+              borderWidth={borderWidth}
+              fontSize="1.5rem"
             />
+          </Animation>
+          <PrettyButton
+            variant="warning"
+            type="submit"
+            fontSize="2rem"
+            color="white"
+            backgroundcolor="#800000"
+            bordercolor="#800000"
+            hovercolor="#e60000"
+            text={Constants.FREE_BUTTON_TEXT}
+          />
         </Form>
-        )}
       </Formik>
     </div>
   );
